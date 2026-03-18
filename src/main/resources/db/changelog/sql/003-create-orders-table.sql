e242c2a118152193d6bcf29387346bdc60449fe2
Evgeny Loshonkov <tixanuiop@gmail.com>
2026-03-18 15:29:16 +0300

    changed 003-create-orders-table.sql to sql with not ref to table from another database

--- src/main/resources/db/changelog/sql/003-create-orders-table.sql
+++ src/main/resources/db/changelog/sql/003-create-orders-table.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE orders (
     id BIGSERIAL PRIMARY KEY,
-    user_id BIGINT REFERENCES users(id),
+    user_id BIGINT NOT NULL,
     status order_status default 'Awaiting' not null,
     total_price numeric(9,2) not null,
     deleted BOOLEAN default false not null,
